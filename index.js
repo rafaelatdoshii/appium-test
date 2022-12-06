5e176e035ae1c88cf9936b87753cb0d0a3956859
Rafael Delos Santos <rafael.delossantos@doshii.io>
2022-12-06 13:20:55 +1100

    Update index.js settings to work with smart hospo app

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const wdio = require("webdriverio");
 
+const espressoBuildConfig = JSON.stringify({
+  additionalAppDependencies: ["com.google.android.material:material:1.0.0", "androidx.lifecycle:lifecycle-extensions:2.1.0"],
+});
+
 const opts = {
     path: '/wd/hub',
     port: 4723,
     capabilities: {
       platformName: "Android",
       platformVersion: "11",
-      deviceName: "Android Emulator",
-      app: "/Users/rafael.delossantos/Projects/appium-test/ApiDemos-debug.apk",
-      appPackage: "io.appium.android.apis",
-      appActivity: ".view.TextFields",
-      automationName: "Espresso"
+      deviceName: "Emulator",
+      forceEspressoRebuild: true,
+      app: "/directory/to/smarthospo/app.apk",
+      automationName: "Espresso",
+      appPackage: "com.doshii.payattable.dev",
+      appActivity: "com.doshii.payattable.ui.main.MainActivity",
+      fullReset: true,
+      showGradleLog: true,
+      printPageSourceOnFindFailure: true,
+      espressoBuildConfig: espressoBuildConfig
     }
   };
   
   async function main () {
     const client = await wdio.remote(opts);
-
-    const field = await client.$("android.widget.EditText");
-    await field.setValue("Hello World!");
-    const value = await field.getText();
-    assert.strictEqual(value,"Hello World!");
-  
     await client.deleteSession();
   }
   
